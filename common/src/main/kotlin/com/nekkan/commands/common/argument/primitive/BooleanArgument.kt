110b76d3687ed1ce8e843a94f6cef6aa83716bbe
nekkan <nxkkdev@gmail.com>
2020-12-31 08:44:13 -0300

    feat: Create BooleanArgument.kt

---
+++ common/src/main/kotlin/com/nekkan/commands/common/argument/primitive/BooleanArgument.kt
@@ -0,0 +1,23 @@
+package com.nekkan.commands.common.argument.primitive
+
+import com.nekkan.commands.common.argument.ArgumentResult
+import com.nekkan.commands.common.argument.CharSequenceConsumer
+import com.nekkan.commands.common.argument.failure
+import com.nekkan.commands.common.argument.success
+import com.nekkan.commands.common.argument.text.SingleWordArgument
+
+data class BooleanArgument(
+    private val `true`: String = "true",
+    private val `false`: String = "false",
+    private val ignoreCase: Boolean = true
+): SingleWordArgument<Boolean>() {
+
+    override fun CharSequenceConsumer.parse(word: String): ArgumentResult<Boolean> {
+        return when {
+            `true`.equals(word, ignoreCase) -> success(true)
+            `false`.equals(word, ignoreCase) -> success(false)
+            else -> failure("Expected '$`true`' or '$`false`', but '$word' was provided.")
+        }
+    }
+
+}
