61553e2baf8700dedf3273be883e06b0af43a624
nekkan <nxkkdev@gmail.com>
2020-12-31 00:01:06 -0300

    feat: Create StringArgument.kt

---
+++ common/src/main/kotlin/com/nekkan/commands/common/argument/text/StringArgument.kt
@@ -0,0 +1,20 @@
+package com.nekkan.commands.common.argument.text
+
+import com.nekkan.commands.common.argument.ArgumentResult
+import com.nekkan.commands.common.argument.CharSequenceConsumer
+import com.nekkan.commands.common.argument.success
+import com.nekkan.commands.common.argument.unexpectedEnd
+import com.nekkan.commands.common.consumeAll
+import com.nekkan.commands.common.hasEnded
+
+object StringArgument: TextArgument() {
+
+    override fun CharSequenceConsumer.parse(): ArgumentResult<String> {
+        if(hasEnded) {
+            return unexpectedEnd()
+        }
+        consumeAll()
+        return success()
+    }
+
+}
