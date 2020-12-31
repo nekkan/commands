58a1f5b4a2f5bf75c426c659be2ac919acc7ec32
nekkan <nxkkdev@gmail.com>
2020-12-31 00:02:14 -0300

    feat: Add WordArgument to TextArgument.kt

--- common/src/main/kotlin/com/nekkan/commands/common/argument/text/TextArgument.kt
+++ common/src/main/kotlin/com/nekkan/commands/common/argument/text/TextArgument.kt
@@ -3,3 +3,5 @@ package com.nekkan.commands.common.argument.text
 import com.nekkan.commands.common.argument.ConsumerArgument
 
 abstract class TextArgument: ConsumerArgument<String>()
+
+abstract class WordArgument: TextArgument()
