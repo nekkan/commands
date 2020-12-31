ad106f62fae40679193a8f31c3d58eb70e00c75c
nekkan <nxkkdev@gmail.com>
2020-12-31 08:45:01 -0300

    feat: Create LongArgument.kt

---
+++ common/src/main/kotlin/com/nekkan/commands/common/argument/primitives/LongArgument.kt
@@ -0,0 +1,18 @@
+package com.nekkan.commands.common.argument.primitives
+
+import com.nekkan.commands.common.argument.ArgumentResult
+import com.nekkan.commands.common.argument.CharSequenceConsumer
+import com.nekkan.commands.common.argument.failure
+import com.nekkan.commands.common.argument.success
+import com.nekkan.commands.common.argument.text.SingleWordArgument
+
+object LongArgument: SingleWordArgument<Long>() {
+
+    override fun CharSequenceConsumer.parse(word: String): ArgumentResult<Long> {
+        return when(val number = word.toLongOrNull()) {
+            null -> failure("A number was expected, but '$word' was provided.")
+            else -> success(number)
+        }
+    }
+
+}
